b9715c1bf55aec27ac6538d8fe3931d391e1b124
Ali Nooner <alinooner@gmail.com>
2021-10-22 16:32:38 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,20 @@
+var famousPerson = "Nicki Minaj";
+var isActive = true;
+
+var famousPerson = "Cardi B";
+
+var num3 = 3;
+var num9 = 9;
+
+var sum = (num3 + num9);
+console.log(sum)
+
+var animals = ["Parrot", "Flamingo", "Otter"]
+
+// Will remove the first animal and return the first animal
+console.log(animals.shift());
+
+// Will remove the first animal and return the remainder of the list
+console.log(animals.slice(1))
+
+console.log(`${famousPerson.length}`);
