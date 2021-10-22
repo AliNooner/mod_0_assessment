ec1da536f9a2dd00c15e31c101daaa1eaaa2ad4e
Ali Nooner <alinooner@gmail.com>
2021-10-22 16:45:08 -0500

    Add modelA

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+// Model of an Airport
+var Airport = {
+  airportName : "MSP International",
+  color : "grey",
+  pilotNames : ["Ali", "Sarah", "Kristen"],
+  foodOptions : ["tacos", "pizza", "sushi"],
+  isFull : true,
+  isBoring : false,
+  numberOfPlanes : 100,
+  numberOfTerminals : 2
+}
