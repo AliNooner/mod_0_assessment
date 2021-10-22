767869f308169eae16fc7744cbbfd1755f5c17fe
Ali Nooner <alinooner@gmail.com>
2021-10-22 16:51:41 -0500

    Add modelB

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+// Model of a Vehicle: (Jeep)
+
+var Jeep = {
+  motto : "Go anywhere do anything",
+  ownerGreeting : "The Jeep wave",
+  isGreatForCamping : true,
+  isOverrated : false,
+  color : ["white", "grey", "black"],
+  waysToCustomize : ["lift kit", "LED lights", "tire color"],
+  numberOfTires : 4,
+  numberOfDoors : 2
+}
