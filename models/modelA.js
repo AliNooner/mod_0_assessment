113d8bd56ddfbf7a5b131eacf3e9512b70d936c7
Ali Nooner <alinooner@gmail.com>
2021-10-24 18:35:57 -0500

    Add ModelA Revisions

--- models/modelA.js
+++ models/modelA.js
@@ -1,11 +1,11 @@
 // Model of an Airport
-var Airport = {
-  airportName : "MSP International",
-  color : "grey",
-  pilotNames : ["Ali", "Sarah", "Kristen"],
-  foodOptions : ["tacos", "pizza", "sushi"],
-  isFull : true,
-  isBoring : false,
-  numberOfPlanes : 100,
-  numberOfTerminals : 2
+var airport = {
+  airportName: "MSP International",
+  color: "grey",
+  pilotNames: ["Ali", "Sarah", "Kristen"],
+  foodOptions: ["tacos", "pizza", "sushi"],
+  isFull: true,
+  isBoring: false,
+  numberOfPlanes: 100,
+  numberOfTerminals: 2
 }
