0913629fb7ecf9f0ff8b476493aae7be8581fca8
Ali Nooner <alinooner@gmail.com>
2021-10-24 18:37:03 -0500

    Add variables revisions

--- variables.js
+++ variables.js
@@ -6,7 +6,7 @@ var famousPerson = "Cardi B";
 var num3 = 3;
 var num9 = 9;
 
-var sum = (num3 + num9);
+var sum = num3 + num9;
 console.log(sum)
 
 var animals = ["Parrot", "Flamingo", "Otter"]
@@ -17,4 +17,4 @@ console.log(animals.shift());
 // Will remove the first animal and return the remainder of the list
 console.log(animals.slice(1))
 
-console.log(`${famousPerson.length}`);
+console.log(famousPerson.length);
